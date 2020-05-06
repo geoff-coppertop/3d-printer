ee9a895173af2249f490098e2fccce98f84a72d2
Geoffrey Thomas <geoff.coppertop@gmail.com>
2020-05-05 19:43:36 -0600

    Functional cleanup
    This 'should' build a functional unit that spins up a kiosk with touch
    enabled and creates a WiFi AP if no network connection exists.

--- hypriot/setup.sh
+++ hypriot/setup.sh
@@ -12,21 +12,26 @@ rm -rf hyperpixel4
 
 # echo "gpu_mem=128" >> /boot/config.txt
 # Replace gpu_mem value with a larger value so that chromium can run 
-sed -i 's/\(gpu_mem=\)\([0-9]*\)/\1128/' /boot/config.txt
+sed -i 's/\(gpu_mem=\)\([0-9]*\)/\1128/g' /boot/config.txt
 
 # Appropriately swap the touchscreen to match the rotation of the display 
 # applied above
-sed -i 's/\(dtoverlay=hyperpixel4\)/\1,touchscreen-swapped-x-y' /boot/config.txt
+sed -i 's/\(dtoverlay=hyperpixel4\)/\1,touchscreen-swapped-x-y,touchscreen-inverted-x/g' /boot/config.txt
 
 # We're going to swap dhcpd for NetworkManager so we can use wifi-connect
-apt-get update
-apt-get install -y -d network-manager
+# apt-get update
+# apt-get install -y -d network-manager
+
 
 systemctl stop dhcpcd
 systemctl disable dhcpcd
 
-apt-get install -y network-manager
+apt-get purge openresolv dhcpcd5
+
+# apt-get install -y network-manager
 apt-get clean
+
+# sed -i 's/\(managed=\)\(false\)/\1true/g' /etc/NetworkManager/NetworkManager.conf
 
 systemctl enable NetworkManager
 systemctl start NetworkManager
--- x11/x.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOCKFILE=/tmp/.X0-lock
-
-# remove lockfile before launching X.
-
-rm -f $LOCKFILE ;
-openbox &
-X -s 0 -quiet -dpms -nocursor -novtswitch vt2 -background none
